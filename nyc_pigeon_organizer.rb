5437b575bf152a2306e12b25dcfaeb4a60cd5d48
Cristina Murillo <cmurill1@jhu.edu>
2018-08-24 19:17:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash={}
+  data.each { |category, category_hash|
+    category_hash.each { |key, array_values|
+        array_values.each { |bird_name|
+          if new_hash.include?(bird_name)
+            if new_hash[bird_name].include?(category)
+              new_hash[bird_name][category] << key.to_s 
+            else 
+            new_hash[bird_name].merge!({category => [key.to_s]})
+            end 
+          else
+            new_hash[bird_name]= {category => [key.to_s]}
+          end
+        }
+    }
+  }  
+  new_hash 
 end
