1ffd2a224b527ed11a4a0e5ff11acd6af2f5f26e
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 16:10:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash ={}
+  data.each do |info, values|
+    values.each do |attribute, name|
+      name.each do |pigeon|
+        if !new_hash.include?(pigeon)
+          new_hash[pigeon] ={}
+        end
+      if !new_hash[pigeon].include?(info)
+        new_hash[pigeon][info] = []
+      end
+      if !new_hash[pigeon][info].include?(attribute)
+        new_hash[pigeon][info] << attribute.to_s
+      end
+    end
+  end
+end
+  new_hash
 end
