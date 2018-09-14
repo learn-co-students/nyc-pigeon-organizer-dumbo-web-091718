c98545b794ef554369e3dc5271e4e2491fa24d26
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 15:04:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  names = data[:color].values.each_with_object([]) {|x, a| a << x}.flatten
+  new_hash = names.each_with_object({}) {|n,h| h[n] = data.keys.each_with_object({}) {|k,a| a[k] = data[k].map {|y,f| 
+    if f.include?(n)
+      y.to_s
+    else
+      []
+    end
+  }.flatten}}
+  #binding.pry
 end
