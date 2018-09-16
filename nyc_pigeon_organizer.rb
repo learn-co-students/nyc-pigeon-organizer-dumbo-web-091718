98a8aa3ec59140daa1f6ee9816290b0e4f4c0529
Jason Odoom <jasonodoom@gmail.com>
2018-09-16 00:51:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_dic={}
+  data.each do |attri_k, attri_d|
+    attri_d.each do |a_k, name_a|
+      name_a.each do |pigeon|
+        if pigeon_dic.include?(name_a) || pigeon_dic[name_a].include?(attri_k)
+           pigeon_dic[name_a][attri_k] << a_k.to_s
+        else
+          pigeon_dic[name_a].merge!{attri_k => [a_k.to_s]} || pigeon_dic[name_a] = {attri_k => [a_k.to_s]} 
+        end
+  return pigeon_dic
+      end
+    end
+  end  
+end 
+  
